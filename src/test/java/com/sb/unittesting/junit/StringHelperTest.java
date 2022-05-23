e8ff51cdc079d84e7478a473213fc8882f454423
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-05-23 21:32:36 +0530

    Moving two tests in two seperate functions

--- src/test/java/com/sb/unittesting/junit/StringHelperTest.java
+++ src/test/java/com/sb/unittesting/junit/StringHelperTest.java
@@ -5,12 +5,19 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class StringHelperTest {
+	// Test functions should always be public and void
 
 	@Test
-	public void truncateAInFirst2Positions() {
+	public void truncateAInFirst2Positions_doubleA_atStart() {
 		StringHelper helper = new StringHelper();
 		// assertEquals(expected,actual);
-		assertEquals("CD",helper.truncateAInFirst2Positions("AACD"));
+		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+
+	}
+
+	@Test
+	public void truncateAInFirst2Positions_singleA_atStart() {
+		StringHelper helper = new StringHelper();
 		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
 	}
 
