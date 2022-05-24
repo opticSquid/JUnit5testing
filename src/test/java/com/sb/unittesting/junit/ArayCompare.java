3b6aa684bd54ddac18f4604bd5dbca6e754572dc
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-05-24 10:12:59 +0530

    Handling Exceptions

--- src/test/java/com/sb/unittesting/junit/ArayCompare.java
+++ src/test/java/com/sb/unittesting/junit/ArayCompare.java
@@ -9,11 +9,20 @@ import org.junit.jupiter.api.Test;
 class ArayCompare {
 
 	@Test
-	void test() {
-		int[] actual = {1,6,2,9,3};
-		int[] expected = {1,2,3,6,9};
+	public void test() {
+		int[] actual = { 1, 6, 2, 9, 3 };
+		int[] expected = { 1, 2, 3, 6, 9 };
 		Arrays.sort(actual);
 		assertArrayEquals(expected, actual);
+	}
+
+	@Test()
+	public void test_handle_exceptions() {
+		int[] actual = null;
+		assertThrows(NullPointerException.class, () -> {
+			// Arrow function pointing having the code which produces the said exception
+			Arrays.sort(actual);
+		});
 	}
 
 }
