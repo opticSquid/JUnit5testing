c7bfcb8c0b161dd30f75b31a7e974d09e9d4ac8a
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-05-25 08:01:26 +0530

    Parameterized Test

---
+++ src/test/java/com/sb/unittesting/junit/StringHelperParameterizedTest.java
@@ -0,0 +1,56 @@
+package com.sb.unittesting.junit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class StringHelperParameterizedTest {
+	// Test functions should always be public and void
+	private StringHelper helper = new StringHelper();
+
+	@Test
+	public void truncateAInFirst2Positions_doubleA_atStart() {
+
+		// assertEquals(expected,actual);
+		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+
+	}
+
+	@Test
+	public void truncateAInFirst2Positions_singleA_atStart() {
+
+		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+	}
+
+	@Test
+	public void truncateAInFirst2Positions_noA_inWord() {
+		assertEquals("CDEF", helper.truncateAInFirst2Positions("CDEF"));
+	}
+
+	@Test
+	public void truncateAInFirst2Positions_doubleA_atEnd() {
+		assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
+	}
+
+	@Test
+	public void areFirstAndLastTwoCharactersTheSame_noSameLetters() {
+		assertFalse(helper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+	}
+
+	@Test
+	public void areFirstAndLastTwoCharactersTheSame_SameLetters_AtStart_and_End() {
+		assertTrue(helper.areFirstAndLastTwoCharactersTheSame("ABAB"));
+	}
+
+	@Test
+	public void areFirstAndLastTwoCharactersTheSame_only2LetterWord() {
+		assertTrue(helper.areFirstAndLastTwoCharactersTheSame("AB"));
+	}
+
+	@Test
+	public void areFirstAndLastTwoCharactersTheSame_SingleLetter() {
+		assertFalse(helper.areFirstAndLastTwoCharactersTheSame("A"));
+	}
+}
--- src/test/java/com/sb/unittesting/junit/StringHelperTest.java
+++ src/test/java/com/sb/unittesting/junit/StringHelperTest.java
@@ -1,56 +1,35 @@
 package com.sb.unittesting.junit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.runners.Parameterized.Parameters;
 
 class StringHelperTest {
 	// Test functions should always be public and void
 	private StringHelper helper = new StringHelper();
 
-	@Test
-	public void truncateAInFirst2Positions_doubleA_atStart() {
+	/*
+	 * In JUnit till version 4 you needed to put @RunWith(Parameterized.class)
+	 * annotation to the class. But in JUnit 5 you only need to do @Parameterized
+	 * Test to the function running parameterized test
+	 */
+
+	/*Here using @CsvSource we arranged our input and expected value in a comma separated manner
+	 * and taking two  in the testing function arranged as our csv arrangement that is AACD is input and CD is output
+	 * SO String input goes first and String expected goes second 
+	 */
+	@ParameterizedTest
+	@CsvSource({ "AACD, CD", "ACD, CD", "CDEF, CDEF", "CDAA, CDAA" })
+	public void truncateAInFirst2Positions_doubleA_atStart(String input, String expected) {
 
 		// assertEquals(expected,actual);
-		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+		assertEquals(expected, helper.truncateAInFirst2Positions(input));
 
-	}
-
-	@Test
-	public void truncateAInFirst2Positions_singleA_atStart() {
-
-		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
-	}
-
-	@Test
-	public void truncateAInFirst2Positions_noA_inWord() {
-		assertEquals("CDEF", helper.truncateAInFirst2Positions("CDEF"));
-	}
-
-	@Test
-	public void truncateAInFirst2Positions_doubleA_atEnd() {
-		assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
-	}
-
-	@Test
-	public void areFirstAndLastTwoCharactersTheSame_noSameLetters() {
-		assertFalse(helper.areFirstAndLastTwoCharactersTheSame("ABCD"));
-	}
-
-	@Test
-	public void areFirstAndLastTwoCharactersTheSame_SameLetters_AtStart_and_End() {
-		assertTrue(helper.areFirstAndLastTwoCharactersTheSame("ABAB"));
-	}
-
-	@Test
-	public void areFirstAndLastTwoCharactersTheSame_only2LetterWord() {
-		assertTrue(helper.areFirstAndLastTwoCharactersTheSame("AB"));
-	}
-
-	@Test
-	public void areFirstAndLastTwoCharactersTheSame_SingleLetter() {
-		assertFalse(helper.areFirstAndLastTwoCharactersTheSame("A"));
 	}
 }
